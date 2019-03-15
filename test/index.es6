5922ab5c1e3fb64a0b103aa01ec958b496600f04
lemehovskiy <lemehovskiy@gmail.com>
2019-03-15 17:31:56 +0200

    update webpack version

--- test/index.es6
+++ test/index.es6
@@ -1,6 +1,6 @@
 require("./sass/style.scss");
 require("jquery");
-import Counter from "./../build/counter"
+import Counter from "./../dist/counter"
 require('./TweenMax');
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,13 @@
+const path = require('path');
+const NODE_ENV = process.env.NODE_ENV || "development";
+
 module.exports = {
     watch: true,
+    mode: NODE_ENV,
     entry: './src/counter.es6',
     output: {
-        filename: 'build/counter.js',
+        path: path.resolve(__dirname, 'dist'),
+        filename: './counter.js',
         libraryTarget: "umd"
     },
 
